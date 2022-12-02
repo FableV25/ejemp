1cc172190579d0740eba14ca27f442d91534df3f
mendo <mendo@LAPTOP-B19C1U5O>
2022-12-02 15:36:44 -0700

    ejemploss

---
+++ src/operaciones_datos/Operaciones_Datos.java
@@ -0,0 +1,20 @@
+
+package operaciones_datos;
+
+/**
+ *
+ * @author Juana García
+ */
+public class Operaciones_Datos{
+    
+    private static Borrar borrar;
+    private static Consultar consulta;
+    private static Insertar inserta;
+    private static Modificar modifica;
+    
+    
+    public static void main(String[] args) {
+        borrar.setVisible(true); 
+    }
+    
+}
